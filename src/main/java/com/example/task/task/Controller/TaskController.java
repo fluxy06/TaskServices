16e2002c479b34d503b3950255c49f79eb5a3d85
Никита Сорокин <nikitasorokin@MacBook-Air-Nikita-2.local>
2025-11-27 14:40:38 +0300

    create/put/delete requests, fix/post/create/task

--- src/main/java/com/example/task/task/Controller/TaskController.java
+++ src/main/java/com/example/task/task/Controller/TaskController.java
@@ -14,12 +14,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RequestMapping("/tasks")
@@ -46,23 +50,34 @@ public class TaskController {
         public Collection<Task>  getAllTasks() {
             return taskService.getAllTasks();
         }
+
+        @PutMapping("{id}")
+        public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+            try {
+                return taskService.updateTask(id, task);
+            }catch (Exception e) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            }
+        }
         
         
     @PostMapping
-public Task createTask(@RequestBody Task request) {
+public ResponseEntity<String> createTask(@RequestBody Task request) {
     try {
-        return taskService.postCreateTask(
-            request.creatorId(),
-            request.assignedUserId(),
-            request.status(),
-            request.createDateTime(),
-            request.deadlineDate(),
-            request.priority()
-        );
+        return taskService.postCreateTask(request);
     } catch (IllegalArgumentException e) {
         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
 
-
+    @DeleteMapping("{id}")
+        public ResponseEntity<String> deleteTask(@PathVariable Long id) {
+            try {
+                taskService.deleteTask(id);
+                return ResponseEntity.ok("task with id: " + id + " deleted");
+            }catch (Exception e) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            }
+        }
 }
+
--- src/main/java/com/example/task/task/Service/TaskService.java
+++ src/main/java/com/example/task/task/Service/TaskService.java
@@ -10,8 +10,10 @@ import com.example.task.task.Entity.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.LoggerFactory;
+import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
@@ -26,30 +28,32 @@ public class TaskService {
     private final AtomicLong nextId = new AtomicLong(1);
 
 
-    public Task postCreateTask(Long creatorId, Long assignedUserId, Status status,
-                               LocalDate createDateTime, LocalDate deadlineDate, Priority priority) {
+    public ResponseEntity<String> postCreateTask(Task task) {
         try {
-            if (deadlineDate.isBefore(createDateTime)) {
+            if (task.deadlineDate().isBefore(task.createDateTime())) {
                 throw new IllegalArgumentException(
-                    "deadline date: " + deadlineDate + " cannot be before create date: " + createDateTime
-                );} else if (status != status.CREATED) {
+                    "deadline date: " + task.deadlineDate() + " cannot be before create date: " + task.createDateTime()
+                );} else if (task.status() != null && task.status() != Status.CREATED) {
                     throw new IllegalArgumentException(
-                            "status of created task then you created she should be 'created', but this tusk has status: " + status 
+                            "status of created task, then you created should be have 'status: created', but this tusk has status: " + task.status()
+                            + " you can don't use field of status, she generated automatically"
                     );
+                }else if(task.id() != null) {
+                    throw new IllegalArgumentException("id of task is generated automatically");
                 }
-            
-            Long id = nextId.getAndIncrement();
-            Task task  = new Task(
-                id, 
-                creatorId,
-                assignedUserId, 
-                status,
-                createDateTime,
-                deadlineDate,
-                priority
+            var newTask = new Task(
+                nextId.getAndIncrement(),
+                task.creatorId(),
+                task.assignedUserId(),
+                Status.CREATED,
+                task.createDateTime(),
+                task.deadlineDate(),
+                task.priority()
             );
-            tasks.put(id, task);
-            return task;
+
+            tasks.put(newTask.id(), newTask);
+            return ResponseEntity.ok("task with id: " + newTask.id() + "  was created");
+            
         } catch(IllegalArgumentException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
@@ -69,7 +73,56 @@ public class TaskService {
         }
         return task;
     } catch(IllegalArgumentException e ) {
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
     }
+    }
+
+    
+    public Task updateTask(Long id, Task task) {
+            try {
+                if (!tasks.containsKey(id)) {
+                        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task with this id: " + id + ", is't find");
+                }
+                Task updatedTask = new Task(
+                    id,
+                    task.creatorId(),
+                    task.assignedUserId(),
+                    task.status(),
+                    task.createDateTime(),
+                    task.deadlineDate(),
+                    task.priority()
+                );
+                if (validDate(task.createDateTime(), task.deadlineDate())) {
+                    tasks.put(id, updatedTask);
+                    return updatedTask;
+                }else  {
+                    throw new IllegalArgumentException("create date is not can be more then deadline date");
+                }
+            }catch (IllegalArgumentException e) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            }
+                catch(Exception e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+            }
+    }
+
+    public void deleteTask(Long id) {
+        try {
+                if (!tasks.containsKey(id)) {
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "don't found task with id: " + id);
+                }
+                tasks.remove(id);
+        }
+        catch(Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "error when /delete/tasks with id: " + id);
+        }
+    }
+
+    public boolean validDate(LocalDate start, LocalDate end) {
+        if(!end.isBefore(start)) {
+            return true;
+        }else {
+            return false;
+        }
     }
 }
